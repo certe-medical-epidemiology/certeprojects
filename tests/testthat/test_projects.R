8d7b7d50d47b0e052b60bd2bdf3a39b1d2d24048
Matthijs S. Berends <m.s.berends@umcg.nl>
2022-01-03 23:04:59 +0100

    version 1.0

--- tests/testthat/test_projects.R
+++ tests/testthat/test_projects.R
@@ -33,18 +33,26 @@ test_that("project properties work", {
   expect_true(file.create(paste0(project_dir, "Project Something - p123/Analyse.R")))
   
   expect_identical(project_get_folder_full(123),
-                   paste0(normalizePath(paste0(project_dir, "Project Something - p123")), "/"))
+                   gsub("\\\\", "/", 
+                        paste0(normalizePath(paste0(project_dir, "Project Something - p123")), "/"),
+                        fixed = TRUE))
   expect_identical(project_get_folder(123), "Project Something - p123")
   expect_warning(project_get_folder(456))
   expect_identical(suppressWarnings(project_get_folder(456)), NA_character_)
   expect_identical(project_get_file("Analyse.R", 123),
-                   normalizePath(paste0(project_dir, "Project Something - p123/Analyse.R")))
+                   gsub("\\\\", "/", 
+                        normalizePath(paste0(project_dir, "Project Something - p123/Analyse.R")),
+                        fixed = TRUE))
   expect_identical(project_get_file(".*lys.*", 123),
-                   normalizePath(paste0(project_dir, "Project Something - p123/Analyse.R")))
+                   gsub("\\\\", "/", 
+                        normalizePath(paste0(project_dir, "Project Something - p123/Analyse.R")),
+                        fixed = TRUE))
   expect_warning(project_get_file("does not exist", 123))
   expect_identical(suppressWarnings(project_get_file("does not exist", 123)), NA_character_)
   expect_identical(project_set_file("test.csv", 123),
-                   paste0(normalizePath(paste0(project_dir, "Project Something - p123/")), "/test.csv"))
+                   gsub("\\\\", "/",
+                        paste0(normalizePath(paste0(project_dir, "Project Something - p123/")), "/test.csv"),
+                        fixed = TRUE))
 })
 
 test_that("teams works", {
@@ -53,5 +61,11 @@ test_that("teams works", {
   expect_warning(teams("test", title = "Title", subtitle = "Subtitle"))
   expect_identical(class(suppressWarnings(teams("test", title = "Title", subtitle = "Subtitle"))), "list")
   expect_error(suppressWarnings(teams("test", title = "Title", subtitle = "Subtitle", images = "hello")))
-  expect_identical(class(question_mark_image), "character")
+  expect_identical(class(question_mark_image()), "character")
+  expect_warning(teams("test", title = "Title", subtitle = "Subtitle",
+                       items = c(test = "abc",
+                                 "bcd"),
+                       buttons = c("Send mail" = "mailto:test@test.com",
+                                   "Open link" = "https://duckduckgo.com")))
+  expect_error(teams_webhooks())
 })

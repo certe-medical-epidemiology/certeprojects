e1d6cc0f7a12da962f496b1de667ebb4a442510d
Matthijs S. Berends <m.s.berends@umcg.nl>
2022-12-02 13:10:38 +0100

    `set_wd()` update

--- R/addins.R
+++ R/addins.R
@@ -45,23 +45,35 @@ addin9_projects_mine <- function() {
   trello_my_cards()
 }
 
-#' @importFrom rstudioapi showPrompt filesPaneNavigate
-set_wd <- function() {
-  if ("clipr" %in% rownames(utils::installed.packages())) {
-    default <- clipr::read_clip()
-    if (!is.character(default) || length(default) != 1 || !dir.exists(as.character(default)[1L])) {
+#' @importFrom rstudioapi showPrompt filesPaneNavigate showQuestion
+set_wd <- function(new_dir = NULL) {
+  if (is.null(new_dir)) {
+    if ("clipr" %in% rownames(utils::installed.packages())) {
+      default <- clipr::read_clip()
+      if (!is.character(default) || length(default) != 1 || !dir.exists(as.character(default)[1L])) {
+        default <- ""
+      }
+    } else {
       default <- ""
     }
-  } else {
-    default <- ""
+    new_dir <- showPrompt(title = "Set Working Directory",
+                          message = "New Working Directory:",
+                          default = default)
   }
-  new_dir <- showPrompt(title = "Set Working Directory",
-                        message = "New Working Directory:",
-                        default = default)
   if (is.null(new_dir) || !dir.exists(new_dir)) {
     return(invisible())
   }
   setwd(new_dir)
-  rstudioapi::filesPaneNavigate(new_dir)
+  filesPaneNavigate(new_dir)
+  
+  rdata <- paste0(new_dir, "/.Rdata")
+  if (file.exists(rdata)) {
+    if (isTRUE(showQuestion(title = "Load RData",
+                            message = paste0("Also load RData from folder ", basename(new_dir), "?"),
+                            ok = "Yes",
+                            cancel = "No"))) {
+      load(rdata, envir = globalenv())
+    }
+  }
 }
 

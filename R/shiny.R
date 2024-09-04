cec7bfc3c0975f3d206b820ce810a4cefe6db81e
Matthijs Berends <m.berends@certe.nl>
2024-09-04 14:59:23 +0100

    emoji filter for Windows

--- R/shiny.R
+++ R/shiny.R
@@ -978,7 +978,7 @@ consult_add <- function(planner = connect_planner(),
       }
       
       # remove emojis
-      body <- gsub("[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U0001F1E6-\U0001F1FF]", "(emoji)", body)
+      body <- gsub("[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U0001F1E6-\U0001F1FF]", "(emoji)", body, perl = TRUE)
       body <- gsub("^U ontvangt niet vaak e-mail.*?(\n)+", "", body)
       body <- gsub(" ?(\n|[^a-zA-Z0-9.,!?\\(\\)]){3,99} ?", "\n", body)
       body <- gsub("\nOorspronkelijk bericht\n.*", "", body)
